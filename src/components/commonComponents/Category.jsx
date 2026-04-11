e356f5c4b9fce96223e48b228145963cf238d952
basudevkumar <jhulonkumar40@gmail.com>
2026-04-11 10:54:32 +0600

    feat: in Category part aded responsive added

--- src/components/commonComponents/Category.jsx
+++ src/components/commonComponents/Category.jsx
@@ -5,19 +5,32 @@ import { menuCategories } from "../../helper/ProjectArrObj";
 
 const Category = () => {
   return (
-    <section className="pb-[208px]">
+    <section className="pb-[100px] lg:pb-[208px]">
       <Container>
-        <p  className="p6 font-inter text-quaternary text-center">Customer Favorites</p>
-        <h2 className="text-primary h2 font-inter text-center pt-4 pb-[78px]">Popular Catagories</h2>
-        <div className="grid grid-cols-4 gap-x-[38px]">
+        <p className="p6 font-inter text-quaternary text-center">
+          Customer Favorites
+        </p>
+        <h2 className="text-primary h2 font-inter text-center pt-4 pb-10 lg:pb-[78px]">
+          Popular Catagories
+        </h2>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:gap-x-[38px]">
           {menuCategories.map((items, index) => {
             return (
-              <div className="py-10 px-[75px] shadow-2xl relative hover:-translate-y-4 cursor-pointer transition duration-300 ease-in-out rounded-[20px]" key={index}>
+              <div
+                className="py-6 px-6 md:px-10 lg:px-[75px] shadow-2xl relative hover:-translate-y-4 cursor-pointer transition duration-300 ease-in-out rounded-[20px]"
+                key={index}
+              >
                 <figure>
-                  <img src={items.imageUrl} alt={items.imageUrl} />
+                  <img
+                    src={items.imageUrl}
+                    alt={items.imageUrl}
+                    className="w-full"
+                  />
                 </figure>
-             <h1 className="pt-3 pb-2    h6 font-inter text-primary">{items.title}</h1>
-             <p className="text-tarnary2 p3 font-inter">{items.subtitle}</p>
+                <h1 className="pt-3 pb-2 h6 font-inter text-primary">
+                  {items.title}
+                </h1>
+                <p className="text-tarnary2 p3 font-inter">{items.subtitle}</p>
               </div>
             );
           })}
