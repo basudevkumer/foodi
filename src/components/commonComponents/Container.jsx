86eeac971691f9bda203f96d75dc78ec28326004
basudevkumar <jhulonkumar40@gmail.com>
2026-04-11 10:28:02 +0600

    feat: added px container in container component

--- src/components/commonComponents/Container.jsx
+++ src/components/commonComponents/Container.jsx
@@ -1,5 +1,5 @@
 const Container = ({ children }) => {
-  return <div className="max-w-[1320px] mx-auto">{children}</div>;
+  return <div className="max-w-[1320px] mx-auto px-4">{children}</div>;
 };
 
 export default Container;
